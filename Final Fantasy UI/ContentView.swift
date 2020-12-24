0ee23556299dfd95906bd2af2f9427a54e73cb3b
Bruno Alves <alves.bruno945@gmail.com>
2020-12-24 20:22:23 -0300

    Initial Commit

---
+++ Final Fantasy UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Final Fantasy UI
+//
+//  Created by Bruno Silva on 24/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Final Fantasy UI/Final_Fantasy_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_Fantasy_UIApp.swift
+//  Final Fantasy UI
+//
+//  Created by Bruno Silva on 24/12/20.
+//
+
+import SwiftUI
+
+@main
+struct Final_Fantasy_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
