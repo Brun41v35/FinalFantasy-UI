f238698b1b273bfdf00a4f2b03d7ed9ade185fd8
Bruno Alves <alves.bruno945@gmail.com>
2020-12-24 23:55:37 -0300

    Adding character details screen

--- Final Fantasy UI/ContentView.swift
+++ Final Fantasy UI/ContentView.swift
@@ -8,12 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //MARK: - Properties
+    
+    //MARK: - Body
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            //MARK: - Image
+            //MARK: Image Background
+            Image("backNoctis")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            //MARK: Image Character
+            Image("noctis")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 250)
+                .clipShape(Circle())
+                .background(Circle().foregroundColor(.white))
+                .overlay(Circle().stroke(Color.white, lineWidth: 4)).offset(x:0, y: -90)
+                .padding(.bottom, -90)
+                .shadow(radius: 30)
+            
+            //MARK: - Title
+            Text("Noctis")
+                .font(.system(size: 25))
+                .fontWeight(.bold)
+            
+            //MARK: - Spacer
+            Spacer()
+            
+        }//MARK: - VStack
+        .edgesIgnoringSafeArea(.top)
     }
 }
 
+//MARK: - Preview
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
